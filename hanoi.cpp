1847f45831d962221bba1abdc69d36d2f3d8f0d6
gadiaha <164947349+gadiaha@users.noreply.github.com>
2024-03-25 14:10:18 -0700

    Create hanoi.cpp
    Towers of Hanoi

---
+++ hanoi.cpp
@@ -0,0 +1,27 @@
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+void disc_mov(int src, int dest)
+{
+	cout << "Moving disk from " << src << " to " << dest << endl;
+}
+
+void hanoi_mv(int num_discs, int src, int aux, int dest)
+{
+	if (num_discs == 1)
+		disc_mov(src, dest);
+	else {
+		hanoi_mv(num_discs - 1, src, dest, aux);
+		disc_mov(src, dest);
+		hanoi_mv(num_discs - 1, aux, src, dest);
+	}
+}
+
+int main()
+{
+	hanoi_mv(3, 1, 2, 3);
+	
+	system("pause");
+	return 0;
+}
